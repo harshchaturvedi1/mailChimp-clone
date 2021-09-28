0cfd5f6ffe0dfb1a5d86916b45ed32e3b12b0539
harsh <harshchaturvedi589@gmail.com>
2021-09-28 19:23:39 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>mail-chimp clone</h1>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+  rootElement
+);
