520e9a0486fa8b8c75c22758316d12d7b20fd35e
imankush7 <ankushkrsingh96@gmail.com>
2021-09-29 17:58:53 +0530

    change

--- src/components/Dashboard/Card1.jsx
+++ src/components/Dashboard/Card1.jsx
@@ -2,6 +2,6 @@ import './Card1.css'
 
 export default function Card1() {
     return (
-        <img src="./dashboard-images/bake.jpg" alt="bake" />
+        <img className="imgCard1" src="./dashboard-images/bake.jpg" alt="bake" />
     )
 }
