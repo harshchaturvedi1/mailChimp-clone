639d144c00b349c1955e507f705d507a112a1907
imbickydutta <imbickydutta@gmail.com>
2021-09-29 15:13:45 +0530

    "Dashboard Cards"

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -1,18 +1,18 @@
-import {Route,Switch} from "react-router-dom"
-import { DashboardMain } from "../Components/Dashboard/DashboardMain"
-import Homepage from "../Components/Homepage"
-export default function Routers(){
+import { Route, Switch } from "react-router-dom"
+import { DashboardMain } from "../components/Dashboard/DashboardMain"
+import Homepage from "../components/Homepage"
+export default function Routers() {
     return <div>
-            <Switch>
-                <Route exact path="/">
-                    <Homepage/>
+        <Switch>
+            <Route exact path="/">
+                <Homepage />
             </Route>
 
             {/* to dashboard */}
-                <Route exact path="/dashboard">
-                    <DashboardMain />
-                </Route>
-                <Route>Error 404</Route>
-            </Switch>
+            <Route exact path="/dashboard">
+                <DashboardMain />
+            </Route>
+            <Route>Error 404</Route>
+        </Switch>
     </div>
 }
