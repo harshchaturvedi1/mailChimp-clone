7837b69962b203ea4a4476f67b1a8d8b947b7e46
imankush7 <ankushkrsingh96@gmail.com>
2021-09-29 18:09:41 +0530

    little

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -83,7 +83,7 @@ certain aggregate and anonymized data from your browser independent of your cook
   <div className={styles.audience}>Bring your audience data, marketing channels, and insights together so you can reach your goals faster—all from a single platform.</div>
   <div className={styles.secondlast}>
           <div className={styles.slleft}>
-                    <div className={styles.sllf}>Get your business online</div>
+                    <div className={styles.sllf} style={{marginLeft:"2.25rem",paddingLeft:"1rem"}}>Get your business online</div>
                     <div className={styles.sllm}>Give your brand a home with a custom domain. Then launch a website to sell products or take appointments, all with built-in marketing tools to help you boost sales and find fans.</div>
                     <div>
                       <button className={styles.btnsl}>Websites and Stores </button>
@@ -97,7 +97,7 @@ certain aggregate and anonymized data from your browser independent of your cook
   </div>
   <div className={styles.secondlast}>
           <div className={styles.slleft}>
-                    <div className={styles.sllf} style={{marginLeft:"-1rem"}}>Market your business</div>
+                    <div className={styles.sllf} style={{marginLeft:"2rem"}}>Market your business</div>
                     <div className={styles.sllm}>Send the right messages on all the right channels. Build emails, social ads, landing pages, postcards, and more from one place.</div>
                     <div>
                       <button className={styles.btnsl}>Marketing campaigns </button>
