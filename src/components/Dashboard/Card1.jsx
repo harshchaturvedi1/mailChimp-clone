2e69a789999d5cc6d0565add45298405dd41d282
imbickydutta <imbickydutta@gmail.com>
2021-09-30 14:33:03 +0530

    dashboard states

--- src/components/Dashboard/Card1.jsx
+++ src/components/Dashboard/Card1.jsx
@@ -1,7 +1,8 @@
 import './Card1.css'
 
-export default function Card1() {
+export default function Card1({ status }) {
     return (
-        <img className="imgCard1" src="./dashboard-images/bake.jpg" alt="bake" />
+        <img className="imgCard1" src={status ? "./dashboard-images/createwebsite.png" : "./dashboard-images/bake.jpg"} alt="bake" />
     )
 }
+
--- src/components/Dashboard/DashboardMain.jsx
+++ src/components/Dashboard/DashboardMain.jsx
@@ -3,26 +3,39 @@ import Card1 from './Card1'
 import Card2 from './Card2'
 import Card3 from './Card3'
 import Card4 from './Card4'
-
+import { useState } from 'react'
 
 import './DashboardMain.css'
 
 export const DashboardMain = () => {
+
+    const [status, setStatus] = useState(true);
+
     return (
         <>
             <Navbar />
-            <h1 className="dashboard-header">Ready to get Started?</h1>
+            <h1 onClick={() => setStatus(!status)} className="dashboard-header">
+                {status ? "You designed your first email. Nice work" : "Ready to get Started?"}
+            </h1>
             <div className="dashboard-subheader">Here’s what we recommend you do next</div>
-            <Card1 />
-            <Card2 />
-            <div className="card3-1"><Card3 img={
-                "./dashboard-images/Card3_img1.png"} head={"Create a website"} body={"Build a free website with all the tools needed to market your business built right in. No docing skills needed."} button={"Get Started"}
-            /></div>
-            <div className="card3-2"><Card3 img={"./dashboard-images/Card3_img2.png"} head={"Add your contacts "} body={"Your contacts make up your audience. Once you add your contacts, you’ll be able to send your first campaign."} button={"Import Contacts"} /></div>
-            <div className="card3-3"><Card3 img={
-                "./dashboard-images/Card3_img3.png"} head={"Create a sign up form"} body={"Capture contacts and collect the data you need to grow your Mailchimp audience."} button={"Get Started"} /></div>
-            <div className="card3-4"><Card3 img={
-                "./dashboard-images/Card3_img4.png"} head={"Start an automation"} body={"Send timely and perosnalized emails without having to hit the send buttton each time."} button={"Get Started"} /></div>
+            <Card1 status={status} />
+            <Card2 status={status} />
+
+            <div className="dashboard-card3-1-3-2-container">
+
+                <div className="card3-1">{!status ? <Card3 status={status} img={
+                    "./dashboard-images/Card3_img1.png"} head={"Create a website"} body={"Build a free website with all the tools needed to market your business built right in. No docing skills needed."} button={"Get Started"}
+                /> : <Card3 img={
+                    "./dashboard-images/Card3_img4.png"} head={"Start an automation"} body={"Send timely and perosnalized emails without having to hit the send buttton each time."} button={"Get Started"} />}</div>
+                <div className="card3-2"><Card3 img={"./dashboard-images/Card3_img2.png"} head={"Add your contacts "} body={"Your contacts make up your audience. Once you add your contacts, you’ll be able to send your first campaign."} button={"Import Contacts"} /></div>
+            </div>
+
+            <div className={status ? "hide-cards" : "dashboard-card3-3-3-4-container"}>
+                <div status={status} className="card3-3"><Card3 img={
+                    "./dashboard-images/Card3_img3.png"} head={"Create a sign up form"} body={"Capture contacts and collect the data you need to grow your Mailchimp audience."} button={"Get Started"} /></div>
+                <div className="card3-4"><Card3 img={
+                    "./dashboard-images/Card3_img4.png"} head={"Start an automation"} body={"Send timely and perosnalized emails without having to hit the send buttton each time."} button={"Get Started"} /></div>
+            </div>
             <Card4 />
         </>)
 }
