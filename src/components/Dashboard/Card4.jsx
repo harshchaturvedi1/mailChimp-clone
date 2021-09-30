fc9c4f56504a7c2a00dfc7d28e549a6014f62ff2
imbickydutta <imbickydutta@gmail.com>
2021-09-30 07:45:51 +0530

    commit

---
+++ src/components/Dashboard/Card4.jsx
@@ -0,0 +1,13 @@
+import './Card4.css'
+
+export default function Card4() {
+
+    return (
+        <div className="card4-container">
+            <div className="card4-header">This section is your feed. Your most important marketing activity will show up here.</div>
+            <div className="card4-body">Once your campaign is underway, things will look a lot more exciting.</div>
+
+            <button className="card4-button">Create A Campaign</button>
+        </div>
+    )
+}
